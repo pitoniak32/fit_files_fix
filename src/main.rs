eb617fa516d45137188fac7b0cadf2a6959aa755
David Pitoniak <davidpitoniak32@gmail.com>
2023-06-29 13:10:43 -0400

    feat: rework output file path logic

--- src/main.rs
+++ src/main.rs
@@ -28,31 +28,44 @@ async fn main() -> Result<()> {
     };
 
     for file in files {
-        let uri = fitfiletools_api_uri.clone();
-        let output = args.output_file.clone();
-        tokio::spawn(async move {
-            let inputfile_path = std::path::Path::new(&file);
-            log::trace!("check if file exists at: {}", &file);
-            assert!(inputfile_path.exists(), "input file path needs to exist");
+        log::debug!("{file:#?}");
+        // let uri = fitfiletools_api_uri.clone();
+        // let output = args.output_file.clone();
+        // tokio::spawn(async move {
+        //     let inputfile_path = std::path::Path::new(&file);
+        //     log::trace!("check if file exists at: {}", &file);
+        //     assert!(inputfile_path.exists(), "input file path needs to exist");
+        //
+        //     let client = reqwest::blocking::Client::new();
+        //     let res = client
+        //         .post(uri.clone())
+        //         .multipart(multipart::Form::new().file("file", file).unwrap())
+        //         .send()
+        //         .unwrap();
+        //     let json_body: FixedFitFileApiResponse =
+        //         serde_json::from_str(&res.text().unwrap()).unwrap();
+        //     log::debug!("body = {:#?}", json_body);
+        //
+        //
 
-            let client = reqwest::blocking::Client::new();
-            let res = client
-                .post(uri.clone())
-                .multipart(multipart::Form::new().file("file", file).unwrap())
-                .send()
-                .unwrap();
-            let json_body: FixedFitFileApiResponse =
-                serde_json::from_str(&res.text().unwrap()).unwrap();
-            log::debug!("body = {:#?}", json_body);
+        let output_file_path = get_output_file_path(
+            get_output_dir(args.output_dir.clone(), file.clone()),
+            args.output_prefix.clone(),
+            PathBuf::from(file)
+                .file_name()
+                .unwrap()
+                .to_string_lossy()
+                .to_string(),
+            "123abc".to_string(),
+        );
+        log::debug!("out_path: {:#?}", output_file_path,);
 
-            let output_file = format!("{}_{}", output.unwrap().replace(".fit", ""), &json_body.id);
-            log::debug!("file output path = {}", output_file);
-
-            let mut res = client.get(json_body.file).send().unwrap();
-            let mut file = File::create(&output_file).expect("file should be able to be created");
-            std::io::copy(&mut res, &mut file).expect("file was copied");
-            log::info!("file was downloaded to: {}", &output_file);
-        });
+        //
+        //     let mut res = client.get(json_body.file).send().unwrap();
+        //     let mut file = File::create(&output_file).expect("file should be able to be created");
+        //     std::io::copy(&mut res, &mut file).expect("file was copied");
+        //     log::info!("file was downloaded to: {}", &output_file);
+        // });
     }
 
     Ok(())
@@ -67,9 +80,13 @@ struct FixedFitFileApiResponse {
     _ext_data: Option<String>,
 }
 
+/// Usage: this that and the other thing
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[clap(flatten)]
+    pub verbosity: clap_verbosity_flag::Verbosity,
+
     /// enduro2 = 4341
     #[arg(short, long, default_value = "4341")]
     devicetype: u64,
@@ -90,12 +107,14 @@ struct Args {
     #[arg(short, long, requires = "glob_pattern", conflicts_with = "input_file")]
     search_dir: Option<String>,
 
-    /// ex: `/tmp/fit_file` will be downloaded to `/tmp/fit_file_{uuid}.fit`
-    #[arg(short, long)]
-    output_file: Option<String>,
+    /// ex: `fit_file` will be downloaded to `fit_file_{input_file_name}_{uuid}.fit`
+    #[arg(long)]
+    output_prefix: Option<String>,
 
-    #[clap(flatten)]
-    pub verbosity: clap_verbosity_flag::Verbosity,
+    /// ex: `/tmp/fit_file/` will be downloaded to `/tmp/fit_file/{file_path}.fit`
+    /// If this is not provided the input_file parent dir will be used.
+    #[arg(long)]
+    output_dir: Option<String>,
 }
 
 fn expand_paths(search_glob_path: String) -> Vec<String> {
@@ -128,6 +147,31 @@ fn get_search_strategy(
     }
 }
 
+fn get_output_file_path(
+    dir: PathBuf,
+    prefix: Option<String>,
+    file_name: String,
+    id: String,
+) -> PathBuf {
+    dir.join(format!(
+        "{}{}_{}.fit",
+        prefix
+            .and_then(|p| Some(format!("{p}_")))
+            .unwrap_or_default(),
+        file_name.replace(".fit", ""),
+        id
+    ))
+}
+
+fn get_output_dir(output_dir: Option<String>, input_file: String) -> PathBuf {
+    let dir = match output_dir {
+        Some(o_dir) => PathBuf::from(o_dir),
+        None => PathBuf::from(PathBuf::from(input_file).parent().unwrap()),
+    };
+    log::debug!("output_dir: {:#?}", dir);
+    dir
+}
+
 #[cfg(test)]
 mod tests {
     #[cfg(test)]
@@ -136,11 +180,13 @@ mod tests {
 
         #[test]
         fn should_return_none_when_all_params_are_none() {
+            // Arrange / Act / Assert
             assert!(get_search_strategy(None, None, None).is_none())
         }
 
         #[test]
         fn should_return_file_when_dir_and_glob_are_some() {
+            // Arrange / Act / Assert
             assert!(
                 get_search_strategy(Some("/path/search/dir".to_string()), None, None)
                     .is_some_and(|r| { matches!(r, SearchStrategy::File(..)) })
@@ -149,6 +195,7 @@ mod tests {
 
         #[test]
         fn should_return_glob_when_dir_and_glob_are_some() {
+            // Arrange / Act / Assert
             assert!(get_search_strategy(
                 None,
                 Some("/path/search/dir".to_string()),
@@ -171,11 +218,70 @@ mod tests {
             let result = expand_paths(format!(
                 "{}{}*.toml",
                 cwd.to_str().expect("should be able to be converted to str"),
-                std::path::MAIN_SEPARATOR_STR
+                std::path::MAIN_SEPARATOR_STR,
             ));
 
             // Assert
             assert_eq!(result, [cwd.join("Cargo.toml").to_str().unwrap()]);
+        }
+    }
+
+    #[cfg(test)]
+    mod get_output_dir {
+        use super::super::*;
+
+        #[test]
+        fn should_use_output_dir_if_output_dir_is_provided() {
+            // Arrange / Act / Assert
+            assert_eq!(
+                get_output_dir(
+                    Some("path".to_string()),
+                    "/input/file/path/file.fit".to_string(),
+                ),
+                PathBuf::from("path")
+            )
+        }
+
+        #[test]
+        fn should_use_input_file_parent_dir_if_output_dir_is_not_provided() {
+            // Arrange / Act / Assert
+            assert_eq!(
+                get_output_dir(None, "/input/file/path/file.fit".to_string(),),
+                PathBuf::from("/input/file/path/")
+            )
+        }
+    }
+
+    #[cfg(test)]
+    mod get_output_file_path {
+        use super::super::*;
+
+        #[test]
+        fn should_correctly_build_output_file_path_when_prefix_is_provied() {
+            // Arrange / Act / Assert
+            assert_eq!(
+                get_output_file_path(
+                    PathBuf::from("path"),
+                    Some("prefix".to_string()),
+                    "file.fit".to_string(),
+                    "123abc".to_string()
+                ),
+                PathBuf::from("path").join("prefix_file_123abc.fit")
+            )
+        }
+
+        #[test]
+        fn should_correctly_build_output_file_path_when_prefix_is_not_provied() {
+            // Arrange / Act / Assert
+            assert_eq!(
+                get_output_file_path(
+                    PathBuf::from("path"),
+                    None,
+                    "file.fit".to_string(),
+                    "123abc".to_string()
+                ),
+                PathBuf::from("path").join("file_123abc.fit")
+            )
         }
     }
 }
